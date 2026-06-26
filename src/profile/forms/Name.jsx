5edfbcfd74e1e8980ef084a82959ca0038756d8b
ROBBOSoftwareTeamlead <software@robbo.world>
2026-06-26 14:31:12 +0300

    fix(profile): restore full name input and align RU copy
    Show a text field when editing ФИО; update Russian hint and empty
    state to say «ФИО» instead of «имя».

--- src/profile/forms/Name.jsx
+++ src/profile/forms/Name.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
+import { Form } from '@openedx/paragon';
 
 import messages from './Name.messages';
 
@@ -47,7 +48,7 @@ class Name extends React.Component {
 
   render() {
     const {
-      formId, name, visibilityName, editMode, saveState, intl,
+      formId, name, visibilityName, editMode, saveState, error, intl,
     } = this.props;
 
     return (
@@ -58,21 +59,32 @@ class Name extends React.Component {
           editing: (
             <div role="dialog" aria-labelledby={`${formId}-label`}>
               <form onSubmit={this.handleSubmit}>
-                <div className="form-group">
-                  <EditableItemHeader content={intl.formatMessage(messages['profile.name.full.name'])} />
-                  {/*
-                  This isn't a mistake - the name field should not be editable.  But if it were,
-                  you'd find the original code got deleted in the commit which added this comment.
-                  -djoy
-                  TODO: Relatedly, the plumbing for editing the name field is still in place.
-                  Once we're super sure we don't want it back, you could delete the name props and
-                  such to fully get rid of it.
-                  */}
-                  <p data-hj-suppress className="h5">{name}</p>
+                <Form.Group
+                  controlId={formId}
+                  isInvalid={error !== null}
+                >
+                  <label className="edit-section-header" htmlFor={formId} id={`${formId}-label`}>
+                    {intl.formatMessage(messages['profile.name.full.name'])}
+                  </label>
+                  <input
+                    data-hj-suppress
+                    className="form-control"
+                    type="text"
+                    id={formId}
+                    name={formId}
+                    value={name || ''}
+                    onChange={this.handleChange}
+                    aria-describedby={`${formId}-help-text`}
+                  />
+                  {error !== null && (
+                    <Form.Control.Feedback hasIcon={false}>
+                      {error}
+                    </Form.Control.Feedback>
+                  )}
                   <small className="form-text text-muted" id={`${formId}-help-text`}>
                     {intl.formatMessage(messages['profile.name.details'])}
                   </small>
-                </div>
+                </Form.Group>
                 <FormControls
                   visibilityId="visibilityName"
                   saveState={saveState}
@@ -137,6 +149,7 @@ Name.propTypes = {
   visibilityName: PropTypes.oneOf(['private', 'all_users']),
   editMode: PropTypes.oneOf(['editing', 'editable', 'empty', 'static']),
   saveState: PropTypes.string,
+  error: PropTypes.string,
 
   // Actions
   changeHandler: PropTypes.func.isRequired,
@@ -153,6 +166,7 @@ Name.defaultProps = {
   saveState: null,
   name: null,
   visibilityName: 'private',
+  error: null,
 };
 
 export default connect(
